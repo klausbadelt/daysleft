76e6b317b09b5bb6f3f186fdd30e16f9e8c648f4
Klaus Badelt <klaus@kinonation.com>
2017-07-29 19:43:44 +0800

    Return status to Lambda

--- daysleft_lambda.js
+++ daysleft_lambda.js
@@ -18,28 +18,29 @@ var params = {
   Message: {
     Body: {
       Html: {
-        Charset: "UTF-8", 
         Data: "<h1>" + daysleft + "</h1><p>days left to demo day</p>"
       }, 
       Text: {
-        Charset: "UTF-8", 
         Data: daysleft + " days left to demo day."
       }
     },
     Subject: {
-      Charset: "UTF-8", 
       Data: daysleft + " Days Left"
     }
   }
 };
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context, callback) {
   console.log('===SENDING EMAIL===');
   ses.sendEmail(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      callback(err); 
+    }
     else {
       console.log("===EMAIL SENT===");
       console.log(data);           // successful response
+      callback(null, data); 
     }    
   });
 };
