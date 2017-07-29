03004fa022b68c6ba27ec611e3d552bc891a6ac4
Klaus Badelt <klaus@kinonation.com>
2017-07-29 19:26:54 +0800

    Initial commit

---
+++ daysleft_lambda.js
@@ -0,0 +1,45 @@
+'use strict';
+
+console.log('Loading function');
+
+var AWS = require('aws-sdk');
+var ses = new AWS.SES({'region': 'us-east-1'});
+var daysleft = Math.trunc((Date.parse("2017-09-13 00:00 PST+0700")-Date.now())/(1000*60*60*24));
+var params = {
+  Source: "klaus@kinonation.com", 
+  Destination: {
+    BccAddresses: [], 
+    CcAddresses: [], 
+    ToAddresses: [
+      "success@simulator.amazonses.com",
+      "klaus@kinonation.com"
+    ]
+  }, 
+  Message: {
+    Body: {
+      Html: {
+        Charset: "UTF-8", 
+        Data: "<h1>" + daysleft + "</h1><p>days left to demo day</p>"
+      }, 
+      Text: {
+        Charset: "UTF-8", 
+        Data: daysleft + " days left to demo day."
+      }
+    },
+    Subject: {
+      Charset: "UTF-8", 
+      Data: daysleft + " Days Left"
+    }
+  }
+};
+
+exports.handler = function(event, context) {
+  console.log('===SENDING EMAIL===');
+  ses.sendEmail(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {
+      console.log("===EMAIL SENT===");
+      console.log(data);           // successful response
+    }    
+  });
+};
